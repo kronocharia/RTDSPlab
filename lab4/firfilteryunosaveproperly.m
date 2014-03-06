3147f3529aa3648dbbdbb0c6e975fdadcac145d9
Mike Li <mail@mike-li.co.uk>
2014-03-06 14:50:21 +0000

    [Lab4] sighhhhhh

---
+++ lab4/firfilteryunosaveproperly.m
@@ -0,0 +1,27 @@
+F = [230 440 2000 2200];  %tap frequencies
+A = [0 1 0];                    %tap gain, assume its working on
+                                    %pairs of entries, with an implicit 0
+                                    %at the start and end
+pass_ripple = (10^(0.4/20)-1)/(10^(0.4/20)+1); 
+stop_ripple = 10^(-50/20);
+stop_ripple_front = 10^(-67/20);
+DEV = [stop_ripple_front pass_ripple stop_ripple];
+Fs = 8000;                          %Fsamp
+
+[m,f,a,W] = firpmord(F,A,DEV,Fs);   %tells us what order and shit it needs to be
+
+b = firpm(m,f,a, W);                %coeffs of the FIR filter
+[h,w] = freqz(b,1,512);         %freqz(numerator, denominator, resolution)
+plot(f/2 * Fs,a,w/2/pi * Fs,abs(h)) %draws the something....
+
+figure;                             %create new figure window
+freqz(b,1,512, Fs);                 %plot magnitude and phase of filter design
+
+tmp = (b(1));
+str = ['double b[] = {' num2str(tmp)];
+for i=2:length(b)
+    tmp = (b(i));
+    str = [str, ', ', num2str(tmp)];
+end;
+str = [str, '}'];
+
